edc802c3e10f8360a45330e876a286ec633a183c
Kamil <kamil.mosciszko@monterail.com>
2021-05-31 13:40:19 +0200

    UseCase::Tickets::Taken call method got shorter

--- app/concepts/ticket/use_case/taken.rb
+++ app/concepts/ticket/use_case/taken.rb
@@ -3,13 +3,8 @@ module UseCase
       class Taken < UseCase::Base::Base
 
         def call
-          seats_taken=[]
-          i = 0
-          repository.all.each do |ticket|
-            seats_taken[i]=ticket[:seat]
-            i=i+1
-          end
-          return seats_taken
+          repository.all.each_with_index.map { 
+            |ticket, index| [index, ticket[:seat]] }
         end
 
       end
