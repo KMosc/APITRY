0ca910e630702a938d713ce3db0294258d05e465
kamil <kamil.mosciszko@monterail.com>
2021-06-02 01:22:33 +0200

    private validator method for usecase::decorator::buy

--- app/concepts/Payment/wrapper/use_case/buy.rb
+++ app/concepts/Payment/wrapper/use_case/buy.rb
@@ -7,28 +7,36 @@ module UseCase
         def initialize(wrapper)
           @wrapper = wrapper
         end
-        
+
         def call(password, ticket_params, seat, cinema_hall_id, movie_id)
           left = @wrapper.getLeftRepository
           right = @wrapper.getRightRepository
-          @generateseats = UseCase::CinemaHalls::GenerateSeats.new(right)
-          @test = @generateseats.call(cinema_hall_id)
-            if !password.blank? && seat.in?(@test)
-                if (!left.exists?(:cinema_hall_id => cinema_hall_id, seat: seat, movie_id: movie_id))
-                    if left.ticket_available?(cinema_hall_id, movie_id)
-                        if left.exists?(:paid => false)  
-                          left.confirm_reservation(password)
-                        else
-                            attributes = ticket_params.clone
-                            attributes[:paid] = true
-                            @ticket= left.new(attributes)
-                            @ticket.save!
-                        end
-                    end
-                end
-            end
+          if validate?(password, left, right, seat,cinema_hall_id, movie_id)
+            success(left,ticket_params)
+          end
         end
+    
+    private
+
+    def validate?(password, left_Repository, right_Repository, seat, cinema_hall_id, movie_id)
+      @generateseats = UseCase::CinemaHalls::GenerateSeats.new(right_Repository).call(cinema_hall_id)
+      !password.blank? && seat.in?(@generateseats) && (!left_Repository.exists?(:cinema_hall_id => cinema_hall_id, seat: seat, movie_id: movie_id)) && left_Repository.ticket_available?(cinema_hall_id, movie_id)
+    end
+
+    def success(left, ticket_params)
+      if left.exists?(:paid => false)  
+        left.confirm_reservation(password)
+      else
+        attributes = ticket_params.clone
+        attributes[:paid] = true
+        @ticket= left.new(attributes).save!
       end
     end
+
+    end
   end
+end
+
+      
+
   
