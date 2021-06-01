8e0f464f3e90c36679d045534c1250fa3433eb6d
kamil <kamil.mosciszko@monterail.com>
2021-06-01 18:58:59 +0200

    genre model to monteway add

--- app/concepts/genre/genre_repository.rb
+++ app/concepts/genre/genre_repository.rb
@@ -1,9 +1,4 @@
 module Repository
     class GenreRepository  < ::Repository::Base
-        attr_reader :adapter
-
-        def initialize(adapter: Models::Genre)
-            @adapter = adapter
-        end
     end
 end
