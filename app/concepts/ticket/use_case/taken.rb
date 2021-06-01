759ae48624e243a55fee714f52bf6f071044dde8
kamil <kamil.mosciszko@monterail.com>
2021-06-01 11:28:35 +0200

    concepts/ticket/use_case/taken.rb condition add

--- app/concepts/ticket/use_case/taken.rb
+++ app/concepts/ticket/use_case/taken.rb
@@ -4,7 +4,7 @@ module UseCase
         def call(cinema_hall_id, movie_id)
           seats_taken=[]
           i = 0
-          repository.all.each do |ticket|
+          repository.all.where(cinema_hall_id: cinema_hall_id, movie_id: movie_id).each do |ticket|
             seats_taken[i]=ticket[:seat]
             i=i+1
           end
