131a3adc2304b1ad4f72d7927a49f92b2ca11ca6
kamil <kamil.mosciszko@monterail.com>
2021-06-04 20:46:03 +0200

    config/routes.rb fixes

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,20 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'  
   root :to => 'ticket_desks#index'
 
-  resources :ticket_desks do
+  #resources :ticket_desks do
 
-	  resources :cinema_halls do
-      resources :movies do
-        resources :genre
-        resources :ticket
-      end
-	  end
+	 # resources :cinema_halls do
+     # resources :movies do
+      #  resources :genre
+       # resources :ticket
+      #end
+	  #end
+#end
+resources :cinema_halls
+resources :ticket
+resources :genre do
+  resources :movies
 end
-
 scope 'ticket' do
   post "ticket", to: 'ticket#create', as: 'ticket_create'
   post "ticket", to: 'ticket#bookin', as: 'ticket_bookin'
