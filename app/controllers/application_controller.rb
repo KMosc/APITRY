5aa4bbb34779921c444f5c33bdd733e00b0eecc4
kamil <kamil.mosciszko@monterail.com>
2021-06-05 22:26:06 +0200

    Create user works!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,11 @@
-
 class ApplicationController < ActionController::API
-  # Devise code
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # equivalent of authenticate_user! on devise, but this one will check the oauth token
   before_action :doorkeeper_authorize!
-  
 
-  respond_to :json
+  private
 
-  protected
-
-  # Devise methods
-  # Authentication key(:username) and password field will be added automatically by devise.
-  def configure_permitted_parameters
-    added_attrs = [:email, :first_name, :last_name]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  # helper method to access the current user from the token
+  def current_user
+    @current_user ||= User.find_by(id: doorkeeper_token[:resource_owner_id])
   end
-
-     before_action :doorkeeper_authorize!
-
-    private
-
-    # helper method to access the current user from the token
-    def current_user
-      @current_user ||= User.find_by(id: doorkeeper_token[:resource_owner_id])
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   use_doorkeeper do
     skip_controllers :authorizations, :applications, :authorized_applications
   end
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-}, skip: [:sessions, :password] 
+  resources :users, only: %i[create]
   root :to => 'ticket_desks#index'
 
   mount Sidekiq::Web => '/sidekiq'  
