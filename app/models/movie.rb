70a4086a0c250350630c93d65f250adc35dc1053
Kamil <84378754+KMosc@users.noreply.github.com>
2021-06-08 15:14:50 +0200

    Update app/models/movie.rb
    Co-authored-by: Wojciech Stępniak <wojciech.stepniak@monterail.com>

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-  belongs_to :genre, foreign_key: 'genre_id'
+  belongs_to :genre
   belongs_to :cinema_hall, foreign_key: 'cinema_hall_id'
   has_and_belongs_to_many :tickets, inverse_of: :movie, foreign_key: 'movie_id'
   validates_presence_of :cinema_hall
