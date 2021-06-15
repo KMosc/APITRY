1f42b6f2980b474a0c73e093fee552fcc876d4de
kamil <kamil.mosciszko@monterail.com>
2021-06-15 09:04:21 +0200

    /spec/models/cinema_hall_spec.rb adding

--- spec/models/cinema_hall_spec.rb
+++ spec/models/cinema_hall_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe CinemaHall, type: :model do
+      describe 'validations' do
+        context 'when all params are passed' do
+          it 'creates hall with valid number and capacity' do
+            subject= CinemaHall.create(volume: 100)
+            expect(subject).to be_valid
+          end
+        end
+    end
+    describe 'validations' do
+        context 'when param is string' do
+          it 'not valid' do
+            subject= CinemaHall.create(volume: "X")
+            expect(subject).to_not be_valid
+          end
+        end
+    end
+    describe 'validations' do
+        context 'when param volume is nil' do
+          it 'not valid' do
+            subject= CinemaHall.create(volume: nil)
+            expect(subject).to_not be_valid
+          end
+        end
+    end
 end
 
