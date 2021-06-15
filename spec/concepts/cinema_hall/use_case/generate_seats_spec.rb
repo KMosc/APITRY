4ade2fbe51ef3efd259e3a1be3536f2b556d29ab
kamil <kamil.mosciszko@monterail.com>
2021-06-15 12:39:32 +0200

    spec/concepts/cinema_hall/use_case/generate_seats_spec.rb adding

---
+++ spec/concepts/cinema_hall/use_case/generate_seats_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe  do
+    let(:repository) { Repository::CinemaHallRepository.new() }
+    let(:cinema_hall) { 
+        repository=Repository::CinemaHallRepository.new()
+        UseCase::CinemaHalls::Create.new(repository).call(volume: 100) 
+    }
+    subject { UseCase::CinemaHalls::GenerateSeats.new(repository).call(cinema_hall.id) }
+    describe '.call' do
+        it 'creates cinema_hall matrix representation' do
+            expect(subject).to be_a_kind_of(Array)
+
+        end
+    end
+end
