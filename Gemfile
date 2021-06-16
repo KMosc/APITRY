9a7c659709687dc85f00ca9cf6a7cfe1107756c6
kamil <kamil.mosciszko@monterail.com>
2021-06-16 15:16:25 +0200

    add profile image

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'resque'
 gem 'sidekiq'
 gem "sentry-ruby"
 gem "sentry-rails"
+gem 'shrine', '~> 3.0'
 gem 'scout_apm'
 gem 'devise'
 gem 'doorkeeper'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include ImageUploader::Attachment(:image_data) # adds an `image` virtual attribute
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
