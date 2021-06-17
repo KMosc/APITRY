9b0cbeafcab6b4ccdbdf45e4002cc6c06f418680
kamil <kamil.mosciszko@monterail.com>
2021-06-17 11:48:50 +0200

    profile image add

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  include ImageUploader::Attachment(:image_data) # adds an `image` virtual attribute
-
+  include ImageUploader::Attachment(:image)
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
