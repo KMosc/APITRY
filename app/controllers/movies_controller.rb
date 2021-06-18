7799d69d8225f1bbeb01095c856a74ac058d8d43
kamil <kamil.mosciszko@monterail.com>
2021-06-18 22:56:17 +0200

    add poster to the movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -29,7 +29,7 @@ class MoviesController < ApplicationController
   private
   # Only allow a list of trusted parameters through.
   def movie_params
-    params.permit(:id, :title, :description, :age_restriction, :starts_at, :ends_at, :genre_id, :cinema_hall_id)
+    params.permit(:id, :title, :description, :age_restriction, :starts_at, :ends_at, :genre_id, :cinema_hall_id, :image)
   end
   # Post Request succesful
     def post_success(repository)
